03540db1ff38ea64c30a881a15f1beaa1b40d072
hamada <bhak6150@gmail.com>
2024-11-14 22:49:08 +0100

    Modal

--- assets/JS/categoryDetails.js
+++ assets/JS/categoryDetails.js
@@ -10,7 +10,6 @@ const getProducts = async (page) => {
   const skip = (page - 1 ) * 10;
 
   const data = await axios.get(`https://dummyjson.com/products?limit=10&skip=${skip}`);
-  console.log(data);
 
   return data;
 };
@@ -18,13 +17,12 @@ const getProducts = async (page) => {
 const displayProducts = async (page = 1) => {
   const data = await getProducts(page);
   const numberOfPages = Math.ceil(data.data.total / 10 ) ;
-  console.log(page);
 
 
   const result = data.data.products.map((product) => {
       return `
         <div class='product'> 
-        <img src='${product.thumbnail}' alt='${product.description}'>
+        <img src="${product.thumbnail}" alt="${product.description}" class='images'/>
         <h3>${product.title}</h3>
         <span>${product.price}</span>
         </div>
@@ -57,17 +55,66 @@ const displayProducts = async (page = 1) => {
   paginationLinks += ` <li class="page-item"><button onclick= displayProducts('${parseInt(page) + 1}') 
    class="page-link" >&raquo;</button></li>`;
   }
-
-
-  console.log(paginationLinks);
-  
-
   document.querySelector(".pagination").innerHTML = paginationLinks;
 
 // end   pagination // 
 
-
+modal();
 
 };
 
  displayProducts();
+
+
+ // start Modal // *******************************
+ function modal(){
+
+  let currentIndex = 0;
+
+        const modal = document.querySelector(".my-modal");
+        const closeBtn = document.querySelector(".close-btn");
+        const leftBtn = document.querySelector(".left-btn");
+        const rightBtn = document.querySelector(".right-btn");
+        const images = Array.from( document.querySelectorAll(".images"));
+        console.log(images);
+
+        images.forEach(function(img){
+
+          img.addEventListener("click", function(e){
+            modal.classList.remove('d-none');
+            modal.querySelector("img").setAttribute("src", e.target.src);
+
+          
+        })
+           })
+
+
+      // closeBtn    // *******************************
+
+        closeBtn.addEventListener('click', function(){
+          modal.classList.add('d-none');
+        });
+
+      // right Btn  // *******************************
+
+        rightBtn.addEventListener("click", function(){
+          currentIndex++;
+          if(currentIndex >= images.length) currentIndex = 0;
+          modal.querySelector("img").setAttribute("src", images[currentIndex].src);
+        });
+
+        // left Btn  // *******************************
+
+        leftBtn.addEventListener("click", function(){
+          currentIndex--;
+          if(currentIndex < 0) currentIndex = images.length - 1;
+          modal.querySelector("img").setAttribute("src", images[currentIndex].src);
+        });
+
+
+
+
+
+
+
+}
